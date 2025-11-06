f3579469946c8cbb4849e54558fd6663e6476ea2
ambrojobs <89398005+NikoZ21@users.noreply.github.com>
2025-11-06 17:57:44 +0400

    adasdsa

---
+++ MyClass/script.js
@@ -0,0 +1,12 @@
+Add(1, 2);
+Subtract(1, 2);
+
+async function Add(a, b) {
+  const sum = a + b;
+  console.log(sum);
+}
+
+function Subtract(a, b) {
+  const difference = a - b;
+  console.log(difference);
+}
